95428c6451ffa5733413083b5bdc8864419b27b8
Annette Lau <annette1@ualberta.ca>
2025-08-10 13:03:53 -0600

    Animations for arms to move up and down depending on action taken

--- orbie-v0/firmware/action-animations.h
+++ orbie-v0/firmware/action-animations.h
@@ -8,4 +8,18 @@
 #ifndef ACTION_ANIMATIONS_H
 #define ACTION_ANIMATIONS_H
 
+enum RobotAction {
+    ACTION_FORWARD     = 0,
+    ACTION_LEFT        = 1,
+    ACTION_RIGHT       = 2,
+    ACTION_AGITATED    = 3,
+    ACTION_HAPPY_START = 4,
+    ACTION_HAPPY_STOP  = 5,
+    ACTION_ARMS_DOWN   = 6
+};
+
+void setupArms();
+void executeActionAnimation(int action);
+void updateHappyAnimation();
+
 #endif // ACTION_ANIMATIONS_H
