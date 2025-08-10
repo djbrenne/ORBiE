ab023c60459e0a64da3d608eea2bd497be3a9d9d
Dylan Brenneis <31667268+djbrenne@users.noreply.github.com>
2025-08-09 22:05:30 -0600

    Adds initial firmware framwork

---
+++ orbie-v0/firmware/action-animations.h
@@ -0,0 +1,11 @@
+/*
+    ORBiE v0 Firmware Action Animations
+    Copyright (c) 2025 Dylan Brenneis
+
+    This file contains the action animations for the ORBiE v0 firmware.
+*/
+
+#ifndef ACTION_ANIMATIONS_H
+#define ACTION_ANIMATIONS_H
+
+#endif // ACTION_ANIMATIONS_H
